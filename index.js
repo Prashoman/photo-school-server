19da251c07395751a0adc1a2b06ab229e14bbf23
Badhon <prashomanchakrabarti1@gmail.com>
2023-06-07 13:22:42 +0600

    created post api users and get api user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -23,6 +23,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("photographyDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const { userInfo } = req.body;
+
+      console.log(userInfo);
+      const query = { email: userInfo.email };
+      const existingEmail = await userCollection.findOne(query);
+      if (existingEmail) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      //const query = { $sort: { created_at: 1 } };
+      const result = await userCollection
+        .find()
+        .sort({ created_at: -1 })
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
